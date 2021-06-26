eef1c6d9a7a1303c5dff9163d3b1d370c9f50b33
League Student <league@iMac-18.attlocal.net>
2021-06-25 18:04:51 -0700

    Did some recording and encrypting code
    :bear:

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,19 @@
 package _01_File_Recorder;
 
+import java.io.File;
+import java.io.FileWriter;
+
+import javax.swing.JOptionPane;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+	public static void main(String[] args) {
+		try {
+			String Name = "src/_01_File_Recorder/" + JOptionPane.showInputDialog("Enter name:") + ".txt";
+			FileWriter FW = new FileWriter(Name, new File(Name).exists());
+			FW.write(JOptionPane.showInputDialog("Enter message:") + "\n");
+			FW.close();
+		} catch (Exception E) {
+		}
+	}
 }
--- src/_02_File_Encrypt_Decrypt/FileEncryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileEncryptor.java
@@ -1,5 +1,14 @@
 package _02_File_Encrypt_Decrypt;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
+import java.awt.datatransfer.StringSelection;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+
 public class FileEncryptor {
 	/*
 	 * Encryption is the process of encoding a message or information
@@ -20,4 +29,32 @@ public class FileEncryptor {
 	 * Create a program that takes a message and a key from the user.
 	 * Use the key to shift each letter in the users input and save the final result to a file.
 	 */
+	public static void main(String[] args) {
+		String Final = "";
+		String KeyS = JOptionPane.showInputDialog("Enter the key (only 1 character long):");
+		if (KeyS.length() != 1) {
+			JOptionPane.showMessageDialog(null, "Length is not 1");
+			System.exit(0);
+		}
+		int Key = KeyS.charAt(0);
+		for (char Char : JOptionPane.showInputDialog("Enter string:").toCharArray()) {
+			Final += (char) (Character.getNumericValue(Char) + Key);
+		}
+		JFrame Frame = new JFrame();
+		JPanel Panel = new JPanel();
+		JButton Button = new JButton("Copy to clipboard...");
+		JTextPane Label = new JTextPane();
+		Label.setText(Final);
+		Label.setEditable(false);
+		Button.addActionListener(S -> {
+			Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(Label.getText()), null);
+		});
+		Panel.add(Label);
+		Panel.add(Button);
+		Frame.add(Panel);
+		Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Frame.pack();
+		Frame.setVisible(true);
+		Frame.setSize(500, 250);
+	}
 }
